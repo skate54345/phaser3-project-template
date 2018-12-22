1992b80f383f5927e2486922f9a11a6dba247786
austin <skate54345@gmail.com>
2018-12-22 01:14:55 -0700

    added sample assets

--- src/index.js
+++ src/index.js
@@ -13,22 +13,38 @@ var config = {
 
 var game = new Phaser.Game(config);
 
-function preload ()
-{
-    this.load.image('logo', 'assets/logo.png');
+function preload(){
+    this.load.image('red', 'assets/red.jpg');
+    this.load.image('green', 'assets/green.jpg');
+    this.load.image('startButton', 'assets/start.jpg')
+    //this.load.image('bg', 'assets/bg.jpg');
+    //this.load.image('logo', 'assets/logo.jpg');
 }
 
-function create ()
-{
-    var logo = this.add.image(400, 150, 'logo');
+function create(){
+    var red = this.add.image(400, 150, 'red');
+    var green = this.add.image(400, 150, 'green');
+    var startButton = this.add.image(400, 350, 'startButton');
+    this.physics.addCollider(startButton, )
+    //var logo = this.add.image(400, 150, 'logo');
 
-    this.tweens.add({
-        targets: logo,
-        y: 450,
-        duration: 2000,
-        ease: 'Power2',
-        yoyo: true,
-        loop: -1
-    });
+    // this.tweens.add({
+    //     targets: red,
+    //     y: 450,
+    //     duration: 2000,
+    //     ease: 'Power2',
+    //     yoyo: true,
+    //     loop: -1
+    // });
+
+}
+
+function timer(){
+  //random num between 0 and 7
+  var random = (Math.random() * 7)
+  window.setTimeout()
+}
+
+function showGreen(){
 
 }
